67ecb734f627f713df305642749900582fcd8eda
LucaasHenrique <lucashsouza101@gmail.com>
2026-03-20 13:38:47 -0300

    feat: add questions controller routes

--- src/routes/questions.route.ts
+++ src/routes/questions.route.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from "fastify";
-import { getQuestionsAuthUser, getQuestionById, deleteQuestion, createQuestion, associateTopicQuestion, deleteQuestionTopic } from "@/controllers/question.controller.js";
+import { getQuestionsAuthUser, getQuestionById, deleteQuestion, createQuestion, associateTopicQuestion, deleteQuestionTopic, updateQuestion } from "@/controllers/question.controller.js";
+import { createNote, getNotesByQuestionId } from "@/controllers/notes.controller.js";
 import { authMiddleware } from "@/middleware/auth.js";
 
 export default async function questionsRoutes(app: FastifyInstance) {
@@ -9,9 +10,12 @@ export default async function questionsRoutes(app: FastifyInstance) {
 
         protectRoute.get("/", getQuestionsAuthUser);
         protectRoute.get("/:id", getQuestionById);
+        protectRoute.patch("/:id", updateQuestion);
         protectRoute.delete("/:id", deleteQuestion);
         protectRoute.post("/", createQuestion); 
         protectRoute.post("/:id/topic/", associateTopicQuestion);
         protectRoute.delete("/:id/topic/:topic_id", deleteQuestionTopic);
+        protectRoute.post("/:id/notes", createNote);
+        protectRoute.get("/:id/notes", getNotesByQuestionId);
     });
 }
