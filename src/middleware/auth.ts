9b8d894978f92959f73327b8a0311c9b920a6e57
LucaasHenrique <lucashsouza101@gmail.com>
2026-05-12 22:53:11 -0300

    refactor: remove unused FastifyReply parameter and streamline error handling in authMiddleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,17 +1,18 @@
-import type { FastifyRequest, FastifyReply } from "fastify";
+import type { FastifyRequest } from "fastify";
+import { UnauthorizedError } from "@/errors/http.errors.js";
 import jwt from "jsonwebtoken";
 
-export async function authMiddleware(request: FastifyRequest, reply: FastifyReply) {
+export async function authMiddleware(request: FastifyRequest) {
     const token = request.cookies.access_token;
 
     if (!token) {
-        return reply.status(401).send({ message: "Unauthorized" });
+        throw new UnauthorizedError();
     }
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET!) as { id: string, email: string };
         request.user = decoded;
     } catch {
-        return reply.status(401).send({ message: "Unauthorized" });
+        throw new UnauthorizedError();
     }
 }
